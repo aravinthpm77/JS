348365ca28c3029082574c567517f5cab1d9b0c4
Aravinthpm <aravinth7703@gmail.com>
2023-10-17 16:38:48 +0530

    Some movies js

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 
-console.log("Hello!!!")
-const id= setInterval(()=>{
-    console.log(Math.random());
+// console.log("Hello!!!")
+// const id= setInterval(()=>{
+//     console.log(Math.random());
 
-},500)
-clearInterval(id)
+// },500)
+// clearInterval(id)
 
 
+const movies =[
+    {
+        title: 'MA',
+        score:9.3,
+        year:1981
+    },{
+        title: 'MB',
+        score:9.3,
+        year:1951
+    },{
+        title: 'MC',
+        score:9.1,
+        year:1921
+    },{
+        title: 'EA',
+        score:6.3,
+        year:2021
+    }
+]
+
+
+console.log(movies.some(movie=> movie.year<=2015))
